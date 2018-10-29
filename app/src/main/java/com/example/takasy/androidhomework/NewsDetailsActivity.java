94f3952707dba3ae9447a4ee24fcce37ce6dc723
Nikita <takasy94@gmail.com>
2018-10-29 08:51:34 +0300

    90% progress

--- app/src/main/java/com/example/takasy/androidhomework/NewsDetailsActivity.java
+++ app/src/main/java/com/example/takasy/androidhomework/NewsDetailsActivity.java
@@ -1,8 +1,13 @@
 package com.example.takasy.androidhomework;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class NewsDetailsActivity extends AppCompatActivity {
 
@@ -11,10 +16,24 @@ public class NewsDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_details);
 
-        final TextView textView = findViewById(R.id.testText);
+        final ImageView imageView = findViewById(R.id.details_image);
+        final TextView titleView = findViewById(R.id.details_title);
+        final TextView dateView = findViewById(R.id.details_date);
+        final TextView textView = findViewById(R.id.details_text);
         final NewsItem newsItem = (NewsItem) getIntent().getParcelableExtra("newsItem");
-        final String bodyMessage = newsItem.getFullText();
-        textView.setText(bodyMessage);
+        final Toolbar toolbar = findViewById(R.id.details_toolbar);
+
+        setSupportActionBar(toolbar);
+        final ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+            ab.setTitle(newsItem.getCategory().getName());
+        }
+
+        Glide.with(this).load(newsItem.getImageUrl()).into(imageView);
+        titleView.setText(newsItem.getTitle());
+        dateView.setText(newsItem.getPublishDate().toString());
+        textView.setText(newsItem.getFullText());
 
     }
 }
