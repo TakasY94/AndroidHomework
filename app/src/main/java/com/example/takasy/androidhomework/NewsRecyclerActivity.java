8c20d4d0795da5ee2e3762e8f2d8f70f8bf0f6a4
Nikita <takasy94@gmail.com>
2018-11-01 09:01:04 +0300

    button info almost added

--- app/src/main/java/com/example/takasy/androidhomework/NewsRecyclerActivity.java
+++ app/src/main/java/com/example/takasy/androidhomework/NewsRecyclerActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.ToolbarWidgetWrapper;
+
 
 public class NewsRecyclerActivity extends AppCompatActivity {
 
@@ -19,6 +21,9 @@ public class NewsRecyclerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_list);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar);
 
         RecyclerView list = findViewById(R.id.news_recycler);
         final int columns = getResources().getInteger(R.integer.columns);
